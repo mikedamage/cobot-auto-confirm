df4c5cfa8520eca26f44aefaf24db604c9b241eb
Mike Green <mike.is.green@gmail.com>
2010-11-04 14:37:37 -0400

    corrected my english, duh.

--- authorize.php
+++ authorize.php
@@ -3,7 +3,7 @@
  * Cobot OAuth Callback Script
  */
 
-// NOTE: This script is not really that secure. Don't leave it up any longer than you have to.
+// NOTE: This script is not very secure. Don't leave it up any longer than you have to.
 
 $oauth_token = filter_input(INPUT_GET, 'oauth_token', FILTER_SANITIZE_STRING);
 $oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier', FILTER_SANITIZE_STRING);
