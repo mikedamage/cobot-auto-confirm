3f917eb39dfcebb4112ec85f569990e805dc7094
Mike Green <mike.is.green@gmail.com>
2010-11-04 14:36:13 -0400

    removed cruft from PHP callback script and added security note to it

--- authorize.php
+++ authorize.php
@@ -2,13 +2,12 @@
 /**
  * Cobot OAuth Callback Script
  */
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$oauth_token = filter_input(INPUT_GET, 'oauth_token', FILTER_SANITIZE_STRING);
-	$oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier', FILTER_SANITIZE_STRING);
-} else {
-	$oauth_token = filter_input(INPUT_POST, 'oauth_token', FILTER_SANITIZE_STRING);
-	$oauth_verifier = filter_input(INPUT_POST, 'oauth_verifier', FILTER_SANITIZE_STRING);
-}
+
+// NOTE: This script is not really that secure. Don't leave it up any longer than you have to.
+
+$oauth_token = filter_input(INPUT_GET, 'oauth_token', FILTER_SANITIZE_STRING);
+$oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier', FILTER_SANITIZE_STRING);
+
 echo "OAuth Token: <span class=\"oauth-token\">" . $oauth_token . "</span><br />";
 echo "OAuth Verifier: <span class=\"oauth-secret\">" . $oauth_secret . "</span>";
 ?>
