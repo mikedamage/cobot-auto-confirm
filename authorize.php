46416415e66dacbd063d26e6cc82e37dfd97e6b4
Mike Green <mike.is.green@gmail.com>
2010-11-04 22:08:30 -0400

    fixed incorrect variable name in authorize.php

--- authorize.php
+++ authorize.php
@@ -9,5 +9,5 @@ $oauth_token = filter_input(INPUT_GET, 'oauth_token', FILTER_SANITIZE_STRING);
 $oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier', FILTER_SANITIZE_STRING);
 
 echo "OAuth Token: <span class=\"oauth-token\">" . $oauth_token . "</span><br />";
-echo "OAuth Verifier: <span class=\"oauth-secret\">" . $oauth_secret . "</span>";
+echo "OAuth Verifier: <span class=\"oauth-verifier\">" . $oauth_verifier . "</span>";
 ?>
